a3809f72ae1d51a40c4271f9b88fc2720aef32a3
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-08 06:48:47 -0400

    Create yourbased.sh

---
+++ yourbased.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -ex
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install -y tzdata
+gem install bundler -v 2.0.1
+apt-get install cmake
+# install
+bundle install --without guard --deployment
+# before_script
+mysql -e 'create database chalmersit_test;'
+cp config/secrets.example.yml config/secrets.yml
+RAILS_ENV=test bundle exec rake db:migrate db:seed --trace
+# script
+RAILS_ENV=test bundle exec rspec
